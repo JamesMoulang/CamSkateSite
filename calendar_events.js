7c4e57513b547ea70cda4bfe761fccc1ed6188a0
Jamie Dixon <github@jamie-dixon.co.uk>
2026-04-19 19:17:49 +0100

    Invert the dependency on SESSION_CONFIG to make it easier to test these functions later on

--- calendar_events.js
+++ calendar_events.js
@@ -1,13 +1,13 @@
-const directAccessStratrgy = (event) => {
-  const foundEvent = SESSION_CONFIG[event.summary.toLowerCase()];
+const directAccessStratrgy = (event, sessionConfig) => {
+  const foundEvent = sessionConfig[event.summary.toLowerCase()];
   return foundEvent ? { title: event.summary, ...foundEvent } : null;
 };
 
 // This strategy allows us to define alternative titles for events in the SESSION_CONFIG.
 // This is useful for events like "quads & blades" which might be entered in the calendar with
 // a slightly different title but we still want to match it to the same session configuration.
-const alternativeTitleStrategy = (event) => {
-  const [eventKey, eventValue] = Object.entries(SESSION_CONFIG).find(
+const alternativeTitleStrategy = (event, sessionConfig) => {
+  const result = Object.entries(sessionConfig).find(
     ([key, value]) =>
       value.altTitles &&
       value.altTitles.some(
@@ -15,14 +15,16 @@ const alternativeTitleStrategy = (event) => {
       ),
   );
 
-  return eventValue ? { title: eventKey, ...eventValue } : null;
+  return result ? { title: result[0], ...result[1] } : null;
 };
 
-const applyStrategies = (event) => {
+const applyStrategies = (event, sessionConfig) => {
   const strategies = [directAccessStratrgy, alternativeTitleStrategy];
   for (const strategy of strategies) {
-    const result = strategy(event);
-    if (result) return result;
+    const result = strategy(event, sessionConfig);
+    if (result) {
+      return result;
+    }
   }
   return null;
 };
@@ -32,7 +34,11 @@ const calendarEvents = (function calendarEvents() {
     "648a32abb0a80624c5f98e8e4bfd057578a6aed5110ba2addc6f9496fa9cabb4@group.calendar.google.com";
   const API_KEY = "AIzaSyAbxzGY7irnlqDnG9NwmLuzwVb2Q3tkr3I";
 
-  function getCalendarEvents(params, fallbackEvents = []) {
+  function getCalendarEvents(
+    params,
+    fallbackEvents = [],
+    sessionConfig = SESSION_CONFIG,
+  ) {
     const defaultParams = { key: API_KEY, ...params };
     const queryParams = new URLSearchParams(defaultParams).toString();
     const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?${queryParams}`;
@@ -47,7 +53,7 @@ const calendarEvents = (function calendarEvents() {
       .then((data) => data.items || [])
       .then((events) => {
         return events.reduce((agg, event) => {
-          const eventFromConfig = applyStrategies(event);
+          const eventFromConfig = applyStrategies(event, sessionConfig);
 
           return eventFromConfig
             ? [
